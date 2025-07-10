b692292a8ffd915189ce799cf238f6673df3879f
luojunxuan <junxuan.luo@hooenergy.com>
2025-07-10 18:08:56 +0800

    添加首页内容

---
+++ src/components/Article.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="article-view">
+        <a class="user-info">
+            <img :src="info.profilePhoto" alt="">
+            <span>{{ info.name }}</span>
+        </a>
+        <div class="article-content">
+            <a class="article-content__title">{{ info.title }}</a>
+            <a class="article-content__contentText">{{ info.contentText }}</a>
+        </div>
+        <div class="article-bottom">
+            <div class="operation-b">
+                <p class="operation-b-img">
+                    <img src="https://i-operation.csdnimg.cn/images/8efd18d5d7054f77a81294a14cd80ad5.png" alt="">
+                    <span>阅读 {{ info.read }}</span>
+                </p>
+            </div>
+            <div class="operation-b">
+                <p class="operation-b-img">
+                    <img src="https://i-operation.csdnimg.cn/images/bfd9e0f6b22b460d8e318efd356453f9.png" alt="">
+                    <span>{{ info.like }} 赞</span>
+                </p>
+            </div>
+            <div class="operation-b">
+                <p class="operation-b-img">
+                    <img src="https://i-operation.csdnimg.cn/images/bf18d31bb1704f12afa48198fda79b98.png" alt="">
+                    <span>收藏 {{ info.collect }}</span>
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    info: {
+        type: Object,
+        default: () => ({})
+    }
+});
+</script>
--- src/views/homePage/HomeIndex.vue
+++ src/views/homePage/HomeIndex.vue
@@ -1,9 +1,26 @@
 <template>
     <div class="home__view">
         <div class="top_nav">
-            
+
+        </div>
+        <div class="body_content">
+            <Article :info="info"></Article>
         </div>
     </div>
 </template>
 <script setup>
+import { ref } from 'vue';
+import Article from '@/components/Article.vue';
+
+const info = ref({});
+
+info.value = {
+    profilePhoto:'https://i-avatar.csdnimg.cn/c782ab42aa1a4c2a8ac2790d5ec2a9df.jpg!1',
+    name:'luo',
+    title:'luo',
+    contentText:'luo',
+    read:100,
+    like:5,
+    collect:0,
+};
 </script>
